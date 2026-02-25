2e5d1f5c4542de44b0e4ec33d7e04595efe89735
Adamb7rr <bahrcr7@gmail.com>
2026-02-25 06:40:55 +0200

    exersice

---
+++ Kaggle_learn/python/functions-and-getting-help/exercise.py
@@ -0,0 +1,36 @@
+# def round_to_two_places(num):
+#     """Return the given number rounded to two decimal places. 
+    
+#     >>> round_to_two_places(3.14159)
+#     3.14
+#     """
+#     # Replace this body with your own code.
+#     # ("pass" is a keyword that does literally nothing. We used it as a placeholder
+#     # because after we begin a code block, Python requires at least one line of code)
+#     return round(num, 2)
+
+# # Check your answer
+# q1.check()
+
+# def to_smash(total_candies, friends=3):
+#     """Return the number of leftover candies that must be smashed after distributing
+#     the given number of candies evenly between 3 friends.
+    
+#     >>> to_smash(91)
+#     1
+#     """
+#     return total_candies % friends
+
+# # Check your answer
+# q3.check()
+
+# def ruound_to_two_places(num):
+#     return round(num, 4)
+    
+# ruound_to_two_places(9.9999)
+
+# def f(x):
+#     y = abs(x)
+#     return y
+
+# print(f(5))
