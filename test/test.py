c4e4248c68c48a045e87b67fd3e5f9fabfee0410
Adamb7rr <bahrcr7@gmail.com>
2026-02-25 21:43:50 +0200

    update

--- test/test.py
+++ test/test.py
@@ -1,1 +1,17 @@
-help(str)
+import customtkinter as ctk
+
+def button_callback():
+    print("Button clicked!")
+    label.configure(text="You clicked the button!")
+
+app = ctk.CTk()
+app.geometry("400x240")
+app.title("My Python App")
+
+label = ctk.CTkLabel(app, text="This is 100% Python", fg_color="transparent")
+label.pack(pady=20)
+
+button = ctk.CTkButton(app, text="Click Me", command=button_callback)
+button.pack(pady=20)
+
+app.mainloop()
