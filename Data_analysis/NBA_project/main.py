fffdb510624913125d41ac193487885035146bec
Adamb7rr <bahrcr7@gmail.com>
2026-03-02 22:51:12 +0200

    update

--- Data_analysis/NBA_project/main.py
+++ Data_analysis/NBA_project/main.py
@@ -7,7 +7,3 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 
-data = pd.read_csv(r"F:\py\Data_analysis\NBA_project\all_seasons.csv")
-
-print(data['player_height'].max())
-print(data['player_height'].min())
